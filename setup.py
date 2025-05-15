8854077d5d5b1c008f28b6a16b6f27a607300801
Sachin Hosmani <2821635+sachinhosmani@users.noreply.github.com>
2025-05-15 21:14:55 +0100

    Update setup.py with description and tags and numpy dep

--- setup.py
+++ setup.py
@@ -6,9 +6,21 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'ipython>=7.0.0'
+        'ipython>=7.0.0',
+        'numpy>=1.18.0'
     ],
     package_data={
         'torchvista': ['templates/*.html', 'assets/*'],
     },
+    description="Interactive PyTorch model visualizer for notebooks",
+    long_description=(
+        "Torchvista displays an interactive graph of the forward pass of a PyTorch model directly in the notebook with a single line of code. "
+        "It offers error-tolerant partial visualization to trace the source of errors like tensor shape mismatches"
+    ),
+    long_description_content_type='text/plain',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",        
+    ]
 )
