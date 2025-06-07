d5d9ef3eea00cf4544f164045bfadd75353e75bd
Sachin Hosmani <2821635+sachinhosmani@users.noreply.github.com>
2025-06-07 17:28:42 +0100

    Revamp demos home page; Change collapse_modules_after_depth default to 1

--- torchvista/tracer.py
+++ torchvista/tracer.py
@@ -675,7 +675,7 @@ def plot_graph(adj_list, module_info, func_info, node_to_module_path, parent_mod
     display(HTML(output))
 
 
-def _get_demo_html_str(model, inputs, code_contents, collapse_modules_after_depth=2, show_non_gradient_nodes=True, forced_module_tracing_depth=None):
+def _get_demo_html_str(model, inputs, code_contents, collapse_modules_after_depth=1, show_non_gradient_nodes=True, forced_module_tracing_depth=None):
     collapse_modules_after_depth = max(collapse_modules_after_depth, 0)
     adj_list = {}
     module_info = {}
@@ -723,7 +723,7 @@ def _get_demo_html_str(model, inputs, code_contents, collapse_modules_after_dept
     return output, exception
 
 
-def trace_model(model, inputs, max_module_expansion_depth=None, show_non_gradient_nodes=True, forced_module_tracing_depth=None, collapse_modules_after_depth=2):
+def trace_model(model, inputs, max_module_expansion_depth=None, show_non_gradient_nodes=True, forced_module_tracing_depth=None, collapse_modules_after_depth=1):
     adj_list = {}
     module_info = {}
     func_info = {}
